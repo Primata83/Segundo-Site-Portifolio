4f6711753c3132f8e038a9b635b3b8f75e8d4a56
Hudson Cleiton <hudsoncleiton02@gmail.com>
2024-02-16 00:14:40 -0300

    dds

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Coloque aqui o código para processar os dados do formulário
+  });
+  
